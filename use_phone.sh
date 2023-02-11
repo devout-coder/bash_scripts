8aaf8e599585536ed1564b2f89022e9a97cb1554
devout-coder <awessssomepro@gmail.com>
2023-02-11 22:56:42 +0530

    cut the crap

--- use_phone.sh
+++ use_phone.sh
@@ -1,11 +1,5 @@
-# #!/usr/bin/bash
+#!/bin/bash
 
 ip=$(adb shell ip -f inet addr show | egrep -o '192.*/' | sed 's/.$//')
-adb tcpip 5555
-echo $ip >> damn.txt
-# ip=$(ifconfig | egrep -o 'broadcast 192.*' | egrep -o '[^broadcast ]' | tr -d '\n')
-# ip=$(ifconfig | egrep -o 'broadcast 192.*')
-# ip=$(adb shell ip -f inet addr show | egrep -o '192.*/' | sed 's/.$//')
-# adb shell "ip -f inet addr show"
-# echo $ip
-adb connect $ip:5555
+adb tcpip 5555  
+adb connect $ip:5555  
