b0fe21a99679b2b2dd755e67a1fc9ade092b5bf1
devout-coder <awessssomepro@gmail.com>
2023-02-14 21:30:40 +0530

    switched to absolute path

--- use_phone.sh
+++ use_phone.sh
@@ -3,7 +3,7 @@
 ip=`adb shell ip -f inet addr show | egrep -o '192.*/' | sed 's/.$//'`
 if [ $ip != "adb: no devices/emulators found" ]
 then  
-	echo $ip>./ip.txt
+	echo $ip>~/code_repo/bash_scripts/ip.txt
 	adb tcpip 5555
 	adb connect $ip:5555
 	
