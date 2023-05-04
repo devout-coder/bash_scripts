f3ba5d671226af53bfc62ec340dab8378e983959
Pranil <awessssomepro@gmail.com>
2023-05-05 00:19:53 +0530

    wrote a script to con render

---
+++ con_render.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+curl https://doneify.onrender.com
+
+input="conned render at: "`date`
+logs_path="/home/pranil/code_repo/bash_scripts/logs/con_render_logs.log"
+echo $input >> $logs_path 
--- github_sync.sh
+++ github_sync.sh
@@ -2,15 +2,16 @@
 cd ~/all_synced/ob_notes 
 
 git_st=`git status`
+logs_path="/home/pranil/code_repo/bash_scripts/logs/github_sync_logs.log"
 num=$(awk 'BEGIN{print gsub(ARGV[2],"",ARGV[1])}' "$git_st" "modified")
 if ! [[ "$git_st" == *".obsidian/workspace"* && $num -eq 1 ]]; then
    input="Backup: "`date`
-   echo $input >> ~/code_repo/bash_scripts/logs/github_sync_logs.log
-   git status >> ~/github_sync_logs.log
+   echo $input >> $logs_path
+   git status >> $logs_path 
    git add .
    git commit -m"$input"
-   git push >> ~/github_sync_logs.log
-   echo \n >> ~/github_sync_logs.log
+   git push >> $logs_path 
+   echo \n >> $logs_path 
 fi
 
 
